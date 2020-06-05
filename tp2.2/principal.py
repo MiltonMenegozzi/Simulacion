20b7e8334a6d984b576228db7b7d781a079529ee
juanmdocampo <docampojuan@gmail.com>
2020-06-05 11:23:21 -0300

    distribucion normal

--- tp2.2/principal.py
+++ tp2.2/principal.py
@@ -5,12 +5,15 @@ import pruebas
 
 uniforme = []
 exponencial = []
+normal = []
 
 for i in range(1000):
     uniforme.append(distribuciones.distribucion_uniforme(5,15))
     #exponencial.append(distribuciones.distribucion_exponencial(0.5))
 
 
+for i in range(1000):
+    normal.append(distribuciones.distribucion_normal(0.5,0.2))
 
 #Prueba para ver si la distribucion funciona
 #UNIFORME
@@ -20,3 +23,8 @@ print(pruebas.Kolmogorov(uniforme, 0.05))
 #EXPONENCIAL
 #graficas.histograma(exponencial)
 #pruebas.Anderson(exponencial)
+
+
+#NORMAL
+graficas.histograma(normal)
+print(pruebas.Kolmogorov(normal,0.05))
