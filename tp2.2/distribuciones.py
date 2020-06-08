a6dcc6ae43b3bc9d76da5f416e6726befe7042d0
Nano <ginocarlucci@hotmail.com>
2020-06-08 17:42:02 -0300

    asd

--- tp2.2/distribuciones.py
+++ tp2.2/distribuciones.py
@@ -2,7 +2,7 @@ import numpy as np
 import generador
 import random
 
-numerosGCL = generador.generadorgcl(1234,134775813, 1, pow(2, 32),100000)
+numerosGCL = generador.generadorgcl(1234,134775813, 1, pow(2, 32),10000)
 #numerosBologna = generador.generadorgcl(3298876,134775813,1,pow(2,32),10000)
 
 uniforme = []
@@ -17,7 +17,7 @@ empirica = []
 
 def distribucion_uniforme(a,b):
     for r in numerosGCL:
-        x = a+(b-a)*r #transformo del intervalo (0,1) al intervalo (a,b)
+        x = a+(b-a)*r
         uniforme.append(x)
     return uniforme
 
@@ -28,7 +28,7 @@ def distribucion_exponencial(ex):
     return exponencial
 
 def distribucion_gamma(k,alpha):
-    for i in range(100000):
+    for i in range(10000):
         tr = 1.0
         for i in range(k):
             r = random.choice(numerosGCL)
@@ -38,7 +38,7 @@ def distribucion_gamma(k,alpha):
     return gamma
 
 def distribucion_normal(mu,sigma):
-    for i in range(100000):
+    for i in range(10000):
         sum = 0.0
         for i in range(12):
             r = random.choice(numerosGCL)
@@ -47,9 +47,8 @@ def distribucion_normal(mu,sigma):
         normal.append(x)
     return normal
 
-
 def distribucion_pascal(k,q):
-    for i in range(100000):
+    for i in range(10000):
         tr = 1.0
         qr = np.log(q)
         for i in range(k):
@@ -61,7 +60,7 @@ def distribucion_pascal(k,q):
     return pascal
 
 def distribucion_binomial(n,p):
-    for i in range(100000):
+    for i in range(10000):
         x = 0.0
         for i in range(n):
             r = random.choice(numerosGCL)
@@ -71,7 +70,7 @@ def distribucion_binomial(n,p):
     return binomial
 
 def distribucion_hipergeometrica(tn,ns,p):
-    for i in range(100000):
+    for i in range(10000):
         x = 0.0
         for i in range(ns):
             r = random.choice(numerosGCL)
@@ -82,11 +81,12 @@ def distribucion_hipergeometrica(tn,ns,p):
                 x = x + 1.0
             p = (tn*p-s) / (tn-1.0)
             tn = tn - 1.0
+            if(tn<2):break
         hipergeometrica.append(x)
     return hipergeometrica
 
 def distribucion_poisson(p):
-    for i in range(100000):
+    for i in range(10000):
         x = 0.0
         b = np.exp(-p)
         tr = 1.0
@@ -100,7 +100,7 @@ def distribucion_poisson(p):
 
 def distribucion_empirica():
     p=[0.273,0.037,0.195,0.009,0.124,0.058,0.062,0.151,0.047,0.044]
-    for i in range(100000):
+    for i in range(10000):
         r = random.choice(numerosGCL)
         acum=0
         cont=1
--- tp2.2/principal.py
+++ tp2.2/principal.py
@@ -7,49 +7,31 @@ import scipy.stats as stats
 
 
 """"DISTRIBUCIONES"""
-#uniforme = distribuciones.distribucion_uniforme(0,20)
-#exponencial = distribuciones.distribucion_exponencial(0.5)
+uniforme = distribuciones.distribucion_uniforme(0,20)
+exponencial = distribuciones.distribucion_exponencial(0.5)
 gamma = distribuciones.distribucion_gamma(5,1)
-#normal = distribuciones.distribucion_normal(0.5,0.2)
-#pascal = distribuciones.distribucion_pascal(5,0.4)
-#binomial = distribuciones.distribucion_binomial(20,0.5)
-#hipergeometrica = distribuciones.distribucion_hipergeometrica(40,11,11)
-#poisson = distribuciones.distribucion_poisson(10)
-#empirica = distribuciones.distribucion_empirica()
+normal = distribuciones.distribucion_normal(10,20)
+pascal = distribuciones.distribucion_pascal(5,0.4)
+binomial = distribuciones.distribucion_binomial(20,0.5)
+hipergeometrica = distribuciones.distribucion_hipergeometrica(5000000,500,0.4)
+poisson = distribuciones.distribucion_poisson(10)
+empirica = distribuciones.distribucion_empirica()
 
-
-#Prueba para ver si la distribucion funciona
-
-
-
-#print(pruebas.ChiCuadrado(uniforme, 0.95, 9))
-
-#EXPONENCIAL
-
-#NORMAL
-#normalNumpy = np.random.normal(size=1000)
-#print(stats.kstest(normal,'norm'))
-#print(pruebas.Kolmogorov(normal, 0.05))
-#print(pruebas.ChiCuadrado(normal, 0.95, 9))
-#pruebas.Anderson(normal,'norm')
-
-#GAMMA
-#print(pruebas.Kolmogorov(gamma, 0.05))
-#print(pruebas.ChiCuadrado(gamma, 0.95, 9))
-
+"""GRAFICAS"""
+graficas.histograma(uniforme)
+graficas.histograma(exponencial)
+graficas.histograma(gamma)
+graficas.histograma(normal)
+graficas.histograma(pascal)
+graficas.histograma(binomial)
+graficas.histograma(hipergeometrica)
+graficas.histograma(poisson)
+graficas.histograma(empirica)
 
 """PRUEBAS"""
 #print(pruebas.Kolmogorov(uniforme, 0.05))
 #pruebas.Anderson(exponencial,'expon')
-print(pruebas.KolmogorovScipy(gamma))
+#pruebas.Anderson(normal,'norm') #Funciona para 10_000
 
-"""GRAFICAS"""
-#graficas.histograma(uniforme)
-#graficas.histograma(exponencial)
-#graficas.histograma(gamma)
-#graficas.histograma(normal)
-#graficas.histograma(pascal)
-#graficas.histograma(binomial)
-#graficas.histograma(hipergeometrica)
-#graficas.histograma(poisson)
-#graficas.histograma(empirica)
+
+
